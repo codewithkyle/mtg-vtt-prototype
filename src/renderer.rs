363c6e5651c02628e2ef479f2fcfaad44425025e
Kyle Andrews <codingwithkyle@gmail.com>
2024-06-24 10:41:16 -0400

    Implemented renderer window and resize

--- src/renderer.rs
+++ src/renderer.rs
@@ -208,11 +208,16 @@ impl<'a> Renderer<'a> {
     }
 
     pub fn window(&self) -> &Window {
-        todo!();
+        &self.window
     }
 
     pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        todo!();
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
     }
 
     pub fn update(&mut self) {
